d61bcd32cebb765888f521a2b022a3f32c648503
Esdras Beleza de Noronha <github@contato.esdrasbeleza.com>
2019-01-15 18:16:47 +0000

    Add go project

---
+++ go/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
+
+func main() {
+	name, _ := os.Hostname()
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "[Go] Welcome to my %s!", name)
+	})
+
+	http.ListenAndServe(":80", nil)
+}
